f97af2252babec5b65d4f664b9a84cf13d1f202d
salmaniz82 <salmaniz.82@gmail.com>
2018-11-10 20:13:26 +0500

    readjustments

--- pages/page.php
+++ pages/page.php
@@ -9,7 +9,9 @@
 <?php $page = $data['page'][0]; ?>
 
 		<?php if(isset($page['title'])) { ?>
+
 				<h1 class="page-title" style="color: blue"><?= ucwords($page['title']); ?></h1>
+				
 		<?php }?>
 
 		<p class="desc"><?php echo $page['contents']; ?> </p>

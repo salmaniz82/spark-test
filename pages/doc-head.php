6bb2cddf9e41cfb8ac3c792d7ef3c8c95041af79
salmaniz82 <salmaniz.82@gmail.com>
2018-11-14 04:43:27 +0500

    back to railway

--- pages/doc-head.php
+++ pages/doc-head.php
@@ -4,7 +4,7 @@
     <title> Quantam - <?php if(isset($data['title'])) echo $data['title']; ?></title>
     <link rel='shortcut icon' type='image/ico' href='/favicon.ico' />
 	<link rel="apple-touch-icon" type='image/ico' href="/favicon.ico">
-	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300" rel="stylesheet"
+	<link href="https://fonts.googleapis.com/css?family=Raleway:200" rel="stylesheet">
 	<link rel="stylesheet" type="text/css" href="/assets/css/style.css">
 </head>
 <body>

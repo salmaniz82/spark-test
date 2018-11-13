d7c7c1da1b89f0f353c0c23b2741a2590a3ae5b7
salmaniz82 <salmaniz.82@gmail.com>
2018-11-14 04:40:28 +0500

    text style p.desc added

--- pages/doc-head.php
+++ pages/doc-head.php
@@ -2,8 +2,9 @@
 <html>
 <head>
     <title> Quantam - <?php if(isset($data['title'])) echo $data['title']; ?></title>
-    <link rel="stylesheet" type="text/css" href="/assets/css/style.css">
     <link rel='shortcut icon' type='image/ico' href='/favicon.ico' />
 	<link rel="apple-touch-icon" type='image/ico' href="/favicon.ico">
+	<link href="https://fonts.googleapis.com/css?family=Open+Sans:300" rel="stylesheet"
+	<link rel="stylesheet" type="text/css" href="/assets/css/style.css">
 </head>
 <body>
